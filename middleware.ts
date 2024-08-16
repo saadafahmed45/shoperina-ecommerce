5a06f562da24d988b544bec792bfdf8bb193a550
saadafahmed45 <hafijnahid45@gmail.com>
2024-08-17 02:19:07 +0600

    added

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs";
-import createMiddleware from "next-intl/middleware";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const intlMiddleware = createMiddleware({
-  locales: ["en", "zh"],
-  defaultLocale: "en",
-});
-
-export default authMiddleware({
-  beforeAuth: (req) => {
-    return intlMiddleware(req);
-  },
-
-  publicRoutes: ["/"],
-});
-
-// export default authMiddleware();
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
