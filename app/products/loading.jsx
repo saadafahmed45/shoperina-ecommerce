5312d1f1467f151d091f8d216def5fdbbf9274c0
saadafahmed45 <hafijnahid45@gmail.com>
2024-08-17 01:56:11 +0600

    added the chelk auth

--- app/products/loading.jsx
+++ app/products/loading.jsx
@@ -2,47 +2,8 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="grid grid-cols-4 gap-4">
-      <div className="flex items-center justify-center p-16 h-screen">
-        <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
-          <div className="h-48 rounded-t dark:bg-gray-300"></div>
-          <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-50">
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-3/4 h-6 rounded dark:bg-gray-300"></div>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center p-16 h-screen">
-        <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
-          <div className="h-48 rounded-t dark:bg-gray-300"></div>
-          <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-50">
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-3/4 h-6 rounded dark:bg-gray-300"></div>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center p-16 h-screen">
-        <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
-          <div className="h-48 rounded-t dark:bg-gray-300"></div>
-          <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-50">
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-3/4 h-6 rounded dark:bg-gray-300"></div>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center p-16 h-screen">
-        <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
-          <div className="h-48 rounded-t dark:bg-gray-300"></div>
-          <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-50">
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-full h-6 rounded dark:bg-gray-300"></div>
-            <div className="w-3/4 h-6 rounded dark:bg-gray-300"></div>
-          </div>
-        </div>
-      </div>
+    <div className="flex items-center justify-center p-16 h-screen">
+      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
     </div>
   );
 };
